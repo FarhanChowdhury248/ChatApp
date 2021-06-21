5c09974366e59e00746aa353f755821c21312429
FarhanChowdhury248 <farhanChowdhury248@users.noreply.github.com>
2021-06-20 23:55:55 -0400

    added navigation on session creation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { LandingPage } from "./pages";
+import { LandingPage, SessionPage } from "./pages";
+import { PATHS } from "./shared/constants";
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" exact component={LandingPage} />
+      <Route path={PATHS.LANDING_PAGE} exact component={LandingPage} />
+      <Route path={PATHS.SESSION_PAGE} exact component={SessionPage} />
     </Router>
   );
 };
