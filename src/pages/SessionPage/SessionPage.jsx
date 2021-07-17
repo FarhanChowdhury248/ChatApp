eab7d060483e212111e34d2ccb7e5af68dd575be
Shammo Talukder <shammotalukder4@gmail.com>
2021-07-17 03:59:43 -0400

    Added foundational UI for Session page.

--- src/pages/SessionPage/SessionPage.jsx
+++ src/pages/SessionPage/SessionPage.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import './SessionPage.css';
+import styled from "styled-components";
 
 export const SessionPage = () => {
-  return <h1>Session Page</h1>;
+  return (
+  <Background>
+    <div className="top-info-bar">
+      <h1 className="info-element1">Users: </h1>
+      <h1 className="info-element-main">Welcome to WeChat</h1>
+      <h1 className="info-element2">Room Code: </h1>
+    </div>
+  </Background>
+  );
 };
+
+const Background = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-image: linear-gradient(90deg, #09d3df 0%, #ec0dfc 100%);
+`;
