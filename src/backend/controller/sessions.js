a626a34e0bd29405df290bb3d79d33b29b8d082e
FarhanChowdhury248 <farhanChowdhury248@users.noreply.github.com>
2021-07-29 02:09:36 -0400

    added join session endpoint, refactored session controller

--- src/backend/controller/sessions.js
+++ src/backend/controller/sessions.js
@@ -4,46 +4,97 @@ const generateUniqueId = require("generate-unique-id");
 const Session = require("../models/session.model");
 const Participant = require("../models/participant.model");
 
-router.route("/").get((req, res) => {
-  Session.find()
-    .then((sessions) => res.json(sessions))
-    .catch((err) => res.status(400).json("Error: " + err));
+const getAllSessions = async () => await Session.find().exec();
+
+const createUser = async (username, role = "Guest") => {
+  const newUser = new Participant({
+    _id: mongoose.Types.ObjectId(),
+    name: username,
+    role,
+  });
+
+  await newUser.save();
+  return newUser;
+};
+
+const createSession = async (hostId) => {
+  const newSession = new Session({
+    _id: mongoose.Types.ObjectId(),
+    sessionCode: generateUniqueId({
+      length: 6,
+    }).toString(),
+    members: [hostId],
+  });
+
+  newSession.save();
+  return newSession;
+};
+
+const getSession = async (sessionCode) =>
+  await Session.findOne({ sessionCode }).exec();
+
+const addSessionparticipant = async (sessionCode, newParticipant) => {
+  const session = await getSession(sessionCode);
+  if (!session)
+    throw Error("Invalid session code. Session code does not exist.");
+  session.members = [...session.members, newParticipant];
+  await session.save();
+};
+
+router.route("/").get(async (req, res) => {
+  try {
+    const sessions = await getAllSessions();
+    res.status(200).json({
+      sessions,
+    });
+  } catch (err) {
+    res.status(400).json("Error: " + err);
+  }
 });
 
-router.route("/create").post((req, res) => {
-  const createHost = async () => {
-    const newHost = new Participant({
-      _id: mongoose.Types.ObjectId(),
-      name: req.body.userName,
-      role: "Host",
+router.route("/create").post(async (req, res) => {
+  try {
+    const { userName } = req.body;
+
+    if (userName.length === 0)
+      throw new Error("Invalid username. Username must be non-empty string.");
+
+    const newHost = await createUser(userName, "Host");
+    const newSession = await createSession(newHost._id);
+
+    res.status(200).json({
+      sessionCode: newSession.sessionCode,
+      sessionId: newSession._id,
+      participantId: newHost._id,
     });
+  } catch (err) {
+    res.status(400).json("Error: " + err);
+  }
+});
 
-    const newHostId = await newHost.save().then(() => newHost._id);
-    return { newHostId };
-  };
+router.route("/join").put(async (req, res) => {
+  try {
+    const { sessionCode, username } = req.body;
 
-  (async () => {
-    const { newHostId } = await createHost();
+    if (username.length === 0)
+      throw new Error("Invalid username. Username must be non-empty string.");
+    if (sessionCode.length === 0)
+      throw new Error(
+        "Invalid session code. Session code must be non-empty string."
+      );
 
-    const newSession = new Session({
-      _id: mongoose.Types.ObjectId(),
-      sessionCode: generateUniqueId({
-        length: 6,
-      }).toString(),
-      members: [newHostId],
+    const newParticipant = await createUser(username);
+    addSessionparticipant(sessionCode, newParticipant);
+    const session = await getSession(sessionCode);
+
+    res.status(200).json({
+      sessionCode: session.sessionCode,
+      sessionId: session._id,
+      participantId: newParticipant._id,
     });
-
-    newSession
-      .save()
-      .then(() =>
-        res.status(201).json({
-          sessionCode: newSession.sessionCode,
-          sessionId: newSession._id,
-          participantId: newHostId,
-        })
-      )
-      .catch((err) => res.status(400).json("Error: " + err));
-  })();
+  } catch (err) {
+    res.status(400).json("Error: " + err);
+  }
 });
 
 module.exports = router;
