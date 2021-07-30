c35c5f42359444532625605fa7ddc9f6029aa7fb
FarhanChowdhury248 <farhanChowdhury248@users.noreply.github.com>
2021-07-30 01:46:30 -0400

    added joining sessions and socket updates

--- src/shared/api.js
+++ src/shared/api.js
@@ -2,17 +2,24 @@ import axios from "axios";
 import { API_ROOT } from "./constants";
 
 export const useApi = () => ({
-
-  createParticipant: (userName, role) => 
+  createParticipant: (userName, role) =>
     axios
-      .post(`${API_ROOT}/participants/create`, {"userName": userName, "role": role})
+      .post(`${API_ROOT}/participants/create`, {
+        userName: userName,
+        role: role,
+      })
       .then((res) => res.data)
       .catch((err) => console.error(err)),
-  
-  createSession: (userName) => 
-  axios
-    .post(`${API_ROOT}/sessions/create`, {"userName": userName})
-    .then((res) => res.data)
-    .catch((err) => console.error(err)),
-    
+
+  createSession: (userName) =>
+    axios
+      .post(`${API_ROOT}/sessions/create`, { userName: userName })
+      .then((res) => res.data)
+      .catch((err) => console.error(err)),
+
+  joinSession: (username, sessionCode) =>
+    axios
+      .put(`${API_ROOT}/sessions/join`, { username, sessionCode })
+      .then((res) => res.data)
+      .catch((err) => console.error(err)),
 });
